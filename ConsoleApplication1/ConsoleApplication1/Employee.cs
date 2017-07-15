396a7cd6d6825c20d8f4e3585b39fa4de5225698
James Stigall <James Stigall>
2017-07-15 17:45:24 -0400

    First commit!

---
+++ ConsoleApplication1/ConsoleApplication1/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Employee
+    {
+        public float salary = 40000;
+    }
+
+    public class Programmer : Employee
+    {
+        public float bonus = 10000;
+    }
+
+    class TestInheritance
+    { 
+
+        static void Main(string[] args)
+        {
+            Programmer p1 = new ConsoleApplication1.Programmer();
+
+            Console.WriteLine("Salary: " + p1.salary);
+            Console.WriteLine("Bonus: " + p1.bonus);
+
+            Console.ReadLine();
+        }
+    }
+}
